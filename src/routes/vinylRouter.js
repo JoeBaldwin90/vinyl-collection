83691295b91c5bb121f6eb71ea0404a416bd8f27
Joe Baldwin <jcameronbaldwin@gmail.com>
2020-05-21 16:32:44 +0100

    Refactor routes and controller

--- src/routes/vinylRouter.js
+++ src/routes/vinylRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Vinyl from "../models/vinylModel";
+import controller from "../controller/vinylController";
 
 const router = express.Router();
 
@@ -17,63 +18,10 @@ const getVinyl = async (req, res, next) => {
   });
 };
 
-// Get all
-router.route("/vinyls").get(async (req, res) => {
-  try {
-    const vinyls = await Vinyl.find();
-    res.json(vinyls);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-// Find one
-router.get("/vinyls/:id", getVinyl, (req, res) => {
-  res.json(res.vinyl);
-});
-
-// Add one
-router.post("/vinyls/", async (req, res) => {
-  const vinyl = new Vinyl(req.body);
-  try {
-    const newVinyl = await vinyl.save();
-    res.status(201).json(newVinyl);
-  } catch {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-// Edit one
-router.patch("/vinyls/:id", getVinyl, async (req, res) => {
-  const { vinyl } = res;
-  
-  if (req.body._id) {
-    delete req.body._id;
-  }
-  
-  // Update vinyl object
-  Object.entries(req.body).forEach((item) => {
-    const key = item[0];
-    const value = item[1];
-    vinyl[key] = value;
-  });
-
-  vinyl.save((err) => {
-    if (err) {
-      res.status(400).json({ message: err.message });
-    }
-    return res.json(vinyl);
-  });
-});
-
-// Delete one
-router.delete("/vinyls/:id", getVinyl, async (req, res) => {
-  try {
-    await res.vinyl.remove();
-    res.json({ message: "Deleted vinyl from collection" });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router.get("/vinyls", controller.getAll);
+router.get("/vinyls/:id", getVinyl, controller.getOne);
+router.post("/vinyls/", controller.post);
+router.patch("/vinyls/:id", getVinyl, controller.patch);
+router.delete("/vinyls/:id", getVinyl, controller.delete);
 
 export default router;
