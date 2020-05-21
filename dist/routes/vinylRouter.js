95f59f5ad78dd928903f12ccb354192ca7c073a5
Joe Baldwin <jcameronbaldwin@gmail.com>
2020-05-21 15:36:24 +0100

    Middleware for routes requiring vinyl ID

--- dist/routes/vinylRouter.js
+++ dist/routes/vinylRouter.js
@@ -7,6 +7,8 @@ exports["default"] = void 0;
 
 var _express = _interopRequireDefault(require("express"));
 
+var _mongoose = _interopRequireDefault(require("mongoose"));
+
 var _vinylModel = _interopRequireDefault(require("../models/vinylModel"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
@@ -15,42 +17,154 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-var router = _express["default"].Router();
+var router = _express["default"].Router(); // Middleware to find specific record by request params
 
-router.route("/vinyl").get( /*#__PURE__*/function () {
-  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(req, res) {
-    var vinyls;
+
+var getVinyl = /*#__PURE__*/function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(req, res, next) {
     return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            _context.prev = 0;
-            _context.next = 3;
-            return _vinylModel["default"].find();
+            _vinylModel["default"].findById(req.params.id, function (err, vinyl) {
+              if (err) {
+                return res.status(500).send("Can't find vinyl. Error message: ".concat(err.message));
+              }
 
-          case 3:
-            vinyls = _context.sent;
-            res.json(vinyls);
-            _context.next = 10;
-            break;
+              if (vinyl) {
+                res.vinyl = vinyl;
+                return next();
+              }
 
-          case 7:
-            _context.prev = 7;
-            _context.t0 = _context["catch"](0);
-            res.status(500).json({
-              message: _context.t0.message
+              return res.sendStatus(404);
             });
 
-          case 10:
+          case 1:
           case "end":
             return _context.stop();
         }
       }
-    }, _callee, null, [[0, 7]]);
+    }, _callee);
   }));
 
-  return function (_x, _x2) {
+  return function getVinyl(_x, _x2, _x3) {
     return _ref.apply(this, arguments);
+  };
+}(); // Get all
+
+
+router.route("/vinyls").get( /*#__PURE__*/function () {
+  var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(req, res) {
+    var vinyls;
+    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            _context2.prev = 0;
+            _context2.next = 3;
+            return _vinylModel["default"].find();
+
+          case 3:
+            vinyls = _context2.sent;
+            res.json(vinyls);
+            _context2.next = 10;
+            break;
+
+          case 7:
+            _context2.prev = 7;
+            _context2.t0 = _context2["catch"](0);
+            res.status(500).json({
+              message: _context2.t0.message
+            });
+
+          case 10:
+          case "end":
+            return _context2.stop();
+        }
+      }
+    }, _callee2, null, [[0, 7]]);
+  }));
+
+  return function (_x4, _x5) {
+    return _ref2.apply(this, arguments);
+  };
+}()); // Find one
+
+router.get("/vinyls/:id", getVinyl, function (req, res) {
+  res.json(res.vinyl);
+}); // Add one
+
+router.post("/vinyls/", /*#__PURE__*/function () {
+  var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3(req, res) {
+    var vinyl, newVinyl;
+    return regeneratorRuntime.wrap(function _callee3$(_context3) {
+      while (1) {
+        switch (_context3.prev = _context3.next) {
+          case 0:
+            vinyl = new _vinylModel["default"](req.body);
+            _context3.prev = 1;
+            _context3.next = 4;
+            return vinyl.save();
+
+          case 4:
+            newVinyl = _context3.sent;
+            res.status(201).json(newVinyl);
+            _context3.next = 11;
+            break;
+
+          case 8:
+            _context3.prev = 8;
+            _context3.t0 = _context3["catch"](1);
+            res.status(400).json({
+              message: err.message
+            });
+
+          case 11:
+          case "end":
+            return _context3.stop();
+        }
+      }
+    }, _callee3, null, [[1, 8]]);
+  }));
+
+  return function (_x6, _x7) {
+    return _ref3.apply(this, arguments);
+  };
+}()); // Edit one
+
+router.patch("/vinyls/:id", getVinyl, /*#__PURE__*/function () {
+  var _ref4 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee4(req, res) {
+    return regeneratorRuntime.wrap(function _callee4$(_context4) {
+      while (1) {
+        switch (_context4.prev = _context4.next) {
+          case 0:
+          case "end":
+            return _context4.stop();
+        }
+      }
+    }, _callee4);
+  }));
+
+  return function (_x8, _x9) {
+    return _ref4.apply(this, arguments);
+  };
+}()); // Delete one
+
+router["delete"]("/vinyls/:id", getVinyl, /*#__PURE__*/function () {
+  var _ref5 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee5(req, res) {
+    return regeneratorRuntime.wrap(function _callee5$(_context5) {
+      while (1) {
+        switch (_context5.prev = _context5.next) {
+          case 0:
+          case "end":
+            return _context5.stop();
+        }
+      }
+    }, _callee5);
+  }));
+
+  return function (_x10, _x11) {
+    return _ref5.apply(this, arguments);
   };
 }());
 var _default = router;
--- src/routes/vinylRouter.js
+++ src/routes/vinylRouter.js
@@ -1,9 +1,25 @@
 import express from "express";
-import Vinyl from '../models/vinylModel'
+import mongoose from "mongoose";
+import Vinyl from "../models/vinylModel";
 
 const router = express.Router();
 
-router.route("/vinyl").get(async (req, res) => {
+// Middleware to find specific record by request params
+const getVinyl = async (req, res, next) => {
+  Vinyl.findById(req.params.id, (err, vinyl) => {
+    if (err) {
+      return res.status(500).send(`Can't find vinyl. Error message: ${err.message}`);
+    }
+    if (vinyl) {
+      res.vinyl = vinyl;
+      return next();
+    }
+    return res.sendStatus(404);
+  });
+}
+
+// Get all
+router.route("/vinyls").get(async (req, res) => {
   try {
     const vinyls = await Vinyl.find();
     res.json(vinyls);
@@ -11,5 +27,27 @@ router.route("/vinyl").get(async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+
+// Find one
+router.get("/vinyls/:id", getVinyl, (req, res) => {
+   res.json(res.vinyl);
+});
+
+// Add one
+router.post("/vinyls/", async (req, res) => {
+  const vinyl = new Vinyl(req.body);
+  try {
+    const newVinyl = await vinyl.save();
+    res.status(201).json(newVinyl);
+  } catch {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Edit one
+router.patch("/vinyls/:id", getVinyl, async (req, res) => {});
+
+// Delete one
+router.delete("/vinyls/:id", getVinyl, async (req, res) => {});
 
 export default router;
